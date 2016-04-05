f378d952938c8c3c7318f552d2157279cdaf69be
Most Wanted <bmwant@gmail.com>
2016-04-05 09:15:12 +0300

    More verbose info about lost images

--- utils/find_article_images.py
+++ utils/find_article_images.py
@@ -1,5 +1,10 @@
+import os
+
+import term
+
 from HTMLParser import HTMLParser
 
+from app import config
 from app.models import Post
 
 
@@ -37,6 +42,16 @@ def show_images_statistics():
     """
     Iterate through all articles and find img tags in them
     """
+    def find_missing(files):
+        missing = []
+        for filename in files:
+            path_to_file = os.path.expanduser(
+                    os.path.join(config.STATIC_FOLDER,
+                                 filename.lstrip(os.path.sep)))
+            if not os.path.exists(path_to_file):
+                missing.append(filename)
+        return missing
+
     all_posts = Post.select()
     parser = ImgParser()
     posts_affected = 0
@@ -45,16 +60,26 @@ def show_images_statistics():
         parser.feed(post.post_text)
         if parser._captured_tags:
             images_in_post = len(parser._captured_tags)
-            print('Post #{post_id} has {images_count} images'.format(
+            missing_images = len(find_missing(parser._captured_tags))
+            term.write('Post #{post_id} has {images_count} images '.format(
                 post_id=post.post_id,
                 images_count=images_in_post
             ))
+            if missing_images:
+                term.writeLine('/ {missing_count} missing'.format(
+                        missing_count=missing_images), term.yellow)
+            else:
+                term.writeLine('OK', term.green)
             parser.clear()
             posts_affected += 1
-            images_to_restore += images_in_post
+            images_to_restore += missing_images
 
-    print('Total posts affected %d' % posts_affected)
-    print('Total images to restore %d' % images_to_restore)
+    term.writeLine('Total posts with images %d' % posts_affected, term.bold)
+    if images_to_restore:
+        term.writeLine('Total images to restore %d' % images_to_restore,
+                       term.red)
+    else:
+        term.writeLine('All posts are in good shape!', term.green)
     
     
 if __name__ == '__main__':
