43d5a4689fd9563724a6c30293ed6d7026771659
Misha Behersky <bmwant@gmail.com>
2019-01-22 14:38:35 +0200

    Update docs

--- deploy/scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z $(vagrant box list | grep freebsd)  ]]; then
-    echo "Adding FreeBSD vagrant box"
-    vagrant box add freebsd http://iris.hosting.lv/freebsd-10.1-i386.box
-fi
-
-vagrant up
